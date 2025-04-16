0129ef1cf74c7e0b9cc051ae0efea39d0e5e14ef
Aiden Johnson <arjohnsonn12@gmail.com>
2025-04-16 01:57:59 -0500

    fix: bluriness for paid users

--- src/components/mockEntry.ts
+++ src/components/mockEntry.ts
@@ -22,27 +22,27 @@ const mockEntry = (props: Props) => {
   } = props;
 
   return `<div class="MuiDataGrid-cell--withRenderer MuiDataGrid-cell MuiDataGrid-cell--textLeft MuiDataGrid-withBorderColor" role="cell" data-field="time" data-colindex="0" aria-colindex="1" aria-colspan="1" tabindex="-1" style="min-width: 120px; max-width: 120px; min-height: auto; max-height: none;">
-      <div class="jsx-1989257464 TableCell_TableCell__ofBYs obfuscated" style="margin: inherit;">
+      <div class="jsx-1989257464 TableCell_TableCell__ofBYs " style="margin: inherit;">
         <div class="jsx-1989257464 TableCell_left__Mdoca TableCell_container__RJgLi false ">
-          <div class="jsx-1989257464 obfuscated" style="cursor: initial;">
+          <div class="jsx-1989257464 " style="cursor: initial;">
             <p class="jsx-1989257464">${timeDisplay}</p>
           </div>
         </div>
       </div>
     </div>
     <div class="MuiDataGrid-cell--withRenderer MuiDataGrid-cell MuiDataGrid-cell--textLeft MuiDataGrid-withBorderColor" role="cell" data-field="expiry date" data-colindex="1" aria-colindex="2" aria-colspan="1" tabindex="-1" style="min-width: 120px; max-width: 120px; min-height: auto; max-height: none;">
-      <div class="jsx-1989257464 TableCell_TableCell__ofBYs obfuscated" style="margin: inherit;">
+      <div class="jsx-1989257464 TableCell_TableCell__ofBYs " style="margin: inherit;">
         <div class="jsx-1989257464 TableCell_left__Mdoca TableCell_container__RJgLi false ">
-          <div class="jsx-1989257464 obfuscated" style="cursor: initial;">
+          <div class="jsx-1989257464 " style="cursor: initial;">
             <p class="jsx-1989257464">${expiryDate}</p>
           </div>
         </div>
       </div>
     </div>
     <div class="MuiDataGrid-cell--withRenderer MuiDataGrid-cell MuiDataGrid-cell--textLeft MuiDataGrid-withBorderColor" role="cell" data-field="strike" data-colindex="2" aria-colindex="3" aria-colspan="1" tabindex="-1" style="min-width: 120px; max-width: 120px; min-height: auto; max-height: none;">
-      <div class="jsx-1989257464 TableCell_TableCell__ofBYs obfuscated" style="margin: inherit;">
+      <div class="jsx-1989257464 TableCell_TableCell__ofBYs " style="margin: inherit;">
         <div class="jsx-1989257464 TableCell_nowrap__zqT_T TableCell_millify__lkjHT TableCell_container__RJgLi false ">
-          <div class="jsx-1989257464 obfuscated" style="cursor: initial;">
+          <div class="jsx-1989257464 " style="cursor: initial;">
             <p class="jsx-1989257464">${strike}</p>
           </div>
         </div>
@@ -50,45 +50,45 @@ const mockEntry = (props: Props) => {
     </div>
     <div class="MuiDataGrid-cell--withRenderer MuiDataGrid-cell MuiDataGrid-cell--textLeft MuiDataGrid-withBorderColor" role="cell" data-field="call/put" data-colindex="3" aria-colindex="4" aria-colspan="1" tabindex="-1" style="min-width: 120px; max-width: 120px;
 min-height: auto; max-height: none;">
-      <div class="jsx-1989257464 TableCell_TableCell__ofBYs obfuscated" style="margin: inherit;">
+      <div class="jsx-1989257464 TableCell_TableCell__ofBYs " style="margin: inherit;">
         <div class="jsx-1989257464 TableCell_container__RJgLi false ">
-          <div class="jsx-1989257464 obfuscated" style="cursor: initial;">
+          <div class="jsx-1989257464 " style="cursor: initial;">
             <p class="jsx-1989257464">${type === "C" ? "C" : "P"}</p>
           </div>
         </div>
       </div>
     </div>
     <div class="MuiDataGrid-cell--withRenderer MuiDataGrid-cell MuiDataGrid-cell--textLeft MuiDataGrid-withBorderColor" role="cell" data-field="size" data-colindex="4" aria-colindex="5" aria-colspan="1" tabindex="-1" style="min-width: 120px; max-width: 120px; min-height: auto; max-height: none;">
-      <div class="jsx-1989257464 TableCell_TableCell__ofBYs obfuscated" style="margin: inherit;">
+      <div class="jsx-1989257464 TableCell_TableCell__ofBYs " style="margin: inherit;">
         <div class="jsx-1989257464 TableCell_nowrap__zqT_T TableCell_millify__lkjHT TableCell_container__RJgLi false ">
-          <div class="jsx-1989257464 obfuscated" style="cursor: initial;">
+          <div class="jsx-1989257464 " style="cursor: initial;">
             <p class="jsx-1989257464">${size}</p>
           </div>
         </div>
       </div>
     </div>
     <div class="MuiDataGrid-cell--withRenderer MuiDataGrid-cell MuiDataGrid-cell--textLeft MuiDataGrid-withBorderColor" role="cell" data-field="price" data-colindex="5" aria-colindex="6" aria-colspan="1" tabindex="-1" style="min-width: 120px; max-width: 120px; min-height: auto; max-height: none;">
-      <div class="jsx-1989257464 TableCell_TableCell__ofBYs obfuscated" style="margin: inherit;">
+      <div class="jsx-1989257464 TableCell_TableCell__ofBYs " style="margin: inherit;">
         <div class="jsx-1989257464 TableCell_nowrap__zqT_T TableCell_millify__lkjHT TableCell_container__RJgLi false ">
-          <div class="jsx-1989257464 obfuscated" style="cursor: initial;">
+          <div class="jsx-1989257464 " style="cursor: initial;">
             <p class="jsx-1989257464">${price}</p>
           </div>
         </div>
       </div>
     </div>
     <div class="MuiDataGrid-cell--withRenderer MuiDataGrid-cell MuiDataGrid-cell--textLeft MuiDataGrid-withBorderColor" role="cell" data-field="total value" data-colindex="6" aria-colindex="7" aria-colspan="1" tabindex="-1" style="min-width: 120px; max-width: 120px; min-height: auto; max-height: none;">
-      <div class="jsx-1989257464 TableCell_TableCell__ofBYs obfuscated" style="margin: inherit;">
+      <div class="jsx-1989257464 TableCell_TableCell__ofBYs " style="margin: inherit;">
         <div class="jsx-1989257464 TableCell_nowrap__zqT_T TableCell_millify__lkjHT TableCell_container__RJgLi false ">
-          <div class="jsx-1989257464 obfuscated" style="cursor: initial;">
+          <div class="jsx-1989257464 " style="cursor: initial;">
             <p class="jsx-1989257464">${formattedTotalValue}</p>
           </div>
         </div>
       </div>
     </div>
     <div class="MuiDataGrid-cell--withRenderer MuiDataGrid-cell MuiDataGrid-cell--textLeft MuiDataGrid-withBorderColor" role="cell" data-field="reason" data-colindex="7" aria-colindex="8" aria-colspan="1" tabindex="-1" style="min-width: 120px; max-width: 120px; min-height: auto; max-height: none;">
-      <div class="jsx-1989257464 TableCell_TableCell__ofBYs obfuscated" style="margin: inherit;">
+      <div class="jsx-1989257464 TableCell_TableCell__ofBYs " style="margin: inherit;">
         <div class="jsx-1989257464 TableCell_container__RJgLi false ">
-          <div class="jsx-1989257464 obfuscated" style="cursor: initial;">
+          <div class="jsx-1989257464 " style="cursor: initial;">
             <p class="jsx-1989257464">${reason}</p>
           </div>
         </div>
