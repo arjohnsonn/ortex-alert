e3b83693ba78541dec56af542b3032fc8f0ad365
Aiden Johnson <arjohnsonn12@gmail.com>
2025-04-16 02:21:28 -0500

    fix?: remove blur

--- src/components/mockEntry.ts
+++ src/components/mockEntry.ts
@@ -22,74 +22,74 @@ const mockEntry = (props: Props) => {
   } = props;
 
   return `<div class="MuiDataGrid-cell--withRenderer MuiDataGrid-cell MuiDataGrid-cell--textLeft MuiDataGrid-withBorderColor" role="cell" data-field="time" data-colindex="0" aria-colindex="1" aria-colspan="1" tabindex="-1" style="min-width: 120px; max-width: 120px; min-height: auto; max-height: none;">
-      <div class="jsx-1989257464 TableCell_TableCell__ofBYs " style="margin: inherit;">
-        <div class="jsx-1989257464 TableCell_left__Mdoca TableCell_container__RJgLi false ">
-          <div class="jsx-1989257464 " style="cursor: initial;">
-            <p class="jsx-1989257464">${timeDisplay}</p>
+      <div class=" TableCell_TableCell__ofBYs " style="margin: inherit;">
+        <div class=" TableCell_left__Mdoca TableCell_container__RJgLi false ">
+          <div class=" " style="cursor: initial;">
+            <p class="">${timeDisplay}</p>
           </div>
         </div>
       </div>
     </div>
     <div class="MuiDataGrid-cell--withRenderer MuiDataGrid-cell MuiDataGrid-cell--textLeft MuiDataGrid-withBorderColor" role="cell" data-field="expiry date" data-colindex="1" aria-colindex="2" aria-colspan="1" tabindex="-1" style="min-width: 120px; max-width: 120px; min-height: auto; max-height: none;">
-      <div class="jsx-1989257464 TableCell_TableCell__ofBYs " style="margin: inherit;">
-        <div class="jsx-1989257464 TableCell_left__Mdoca TableCell_container__RJgLi false ">
-          <div class="jsx-1989257464 " style="cursor: initial;">
-            <p class="jsx-1989257464">${expiryDate}</p>
+      <div class=" TableCell_TableCell__ofBYs " style="margin: inherit;">
+        <div class=" TableCell_left__Mdoca TableCell_container__RJgLi false ">
+          <div class=" " style="cursor: initial;">
+            <p class="">${expiryDate}</p>
           </div>
         </div>
       </div>
     </div>
     <div class="MuiDataGrid-cell--withRenderer MuiDataGrid-cell MuiDataGrid-cell--textLeft MuiDataGrid-withBorderColor" role="cell" data-field="strike" data-colindex="2" aria-colindex="3" aria-colspan="1" tabindex="-1" style="min-width: 120px; max-width: 120px; min-height: auto; max-height: none;">
-      <div class="jsx-1989257464 TableCell_TableCell__ofBYs " style="margin: inherit;">
-        <div class="jsx-1989257464 TableCell_nowrap__zqT_T TableCell_millify__lkjHT TableCell_container__RJgLi false ">
-          <div class="jsx-1989257464 " style="cursor: initial;">
-            <p class="jsx-1989257464">${strike}</p>
+      <div class=" TableCell_TableCell__ofBYs " style="margin: inherit;">
+        <div class=" TableCell_nowrap__zqT_T TableCell_millify__lkjHT TableCell_container__RJgLi false ">
+          <div class=" " style="cursor: initial;">
+            <p class="">${strike}</p>
           </div>
         </div>
       </div>
     </div>
     <div class="MuiDataGrid-cell--withRenderer MuiDataGrid-cell MuiDataGrid-cell--textLeft MuiDataGrid-withBorderColor" role="cell" data-field="call/put" data-colindex="3" aria-colindex="4" aria-colspan="1" tabindex="-1" style="min-width: 120px; max-width: 120px;
 min-height: auto; max-height: none;">
-      <div class="jsx-1989257464 TableCell_TableCell__ofBYs " style="margin: inherit;">
-        <div class="jsx-1989257464 TableCell_container__RJgLi false ">
-          <div class="jsx-1989257464 " style="cursor: initial;">
-            <p class="jsx-1989257464">${type === "C" ? "C" : "P"}</p>
+      <div class=" TableCell_TableCell__ofBYs " style="margin: inherit;">
+        <div class=" TableCell_container__RJgLi false ">
+          <div class="" style="cursor: initial;">
+            <p class="">${type === "C" ? "C" : "P"}</p>
           </div>
         </div>
       </div>
     </div>
     <div class="MuiDataGrid-cell--withRenderer MuiDataGrid-cell MuiDataGrid-cell--textLeft MuiDataGrid-withBorderColor" role="cell" data-field="size" data-colindex="4" aria-colindex="5" aria-colspan="1" tabindex="-1" style="min-width: 120px; max-width: 120px; min-height: auto; max-height: none;">
-      <div class="jsx-1989257464 TableCell_TableCell__ofBYs " style="margin: inherit;">
-        <div class="jsx-1989257464 TableCell_nowrap__zqT_T TableCell_millify__lkjHT TableCell_container__RJgLi false ">
-          <div class="jsx-1989257464 " style="cursor: initial;">
-            <p class="jsx-1989257464">${size}</p>
+      <div class=" TableCell_TableCell__ofBYs " style="margin: inherit;">
+        <div class=" TableCell_nowrap__zqT_T TableCell_millify__lkjHT TableCell_container__RJgLi false ">
+          <div class=" " style="cursor: initial;">
+            <p class="">${size}</p>
           </div>
         </div>
       </div>
     </div>
     <div class="MuiDataGrid-cell--withRenderer MuiDataGrid-cell MuiDataGrid-cell--textLeft MuiDataGrid-withBorderColor" role="cell" data-field="price" data-colindex="5" aria-colindex="6" aria-colspan="1" tabindex="-1" style="min-width: 120px; max-width: 120px; min-height: auto; max-height: none;">
-      <div class="jsx-1989257464 TableCell_TableCell__ofBYs " style="margin: inherit;">
-        <div class="jsx-1989257464 TableCell_nowrap__zqT_T TableCell_millify__lkjHT TableCell_container__RJgLi false ">
-          <div class="jsx-1989257464 " style="cursor: initial;">
-            <p class="jsx-1989257464">${price}</p>
+      <div class=" TableCell_TableCell__ofBYs " style="margin: inherit;">
+        <div class=" TableCell_nowrap__zqT_T TableCell_millify__lkjHT TableCell_container__RJgLi false ">
+          <div class=" " style="cursor: initial;">
+            <p class="">${price}</p>
           </div>
         </div>
       </div>
     </div>
     <div class="MuiDataGrid-cell--withRenderer MuiDataGrid-cell MuiDataGrid-cell--textLeft MuiDataGrid-withBorderColor" role="cell" data-field="total value" data-colindex="6" aria-colindex="7" aria-colspan="1" tabindex="-1" style="min-width: 120px; max-width: 120px; min-height: auto; max-height: none;">
-      <div class="jsx-1989257464 TableCell_TableCell__ofBYs " style="margin: inherit;">
-        <div class="jsx-1989257464 TableCell_nowrap__zqT_T TableCell_millify__lkjHT TableCell_container__RJgLi false ">
-          <div class="jsx-1989257464 " style="cursor: initial;">
-            <p class="jsx-1989257464">${formattedTotalValue}</p>
+      <div class=" TableCell_TableCell__ofBYs " style="margin: inherit;">
+        <div class=" TableCell_nowrap__zqT_T TableCell_millify__lkjHT TableCell_container__RJgLi false ">
+          <div class=" " style="cursor: initial;">
+            <p class="">${formattedTotalValue}</p>
           </div>
         </div>
       </div>
     </div>
     <div class="MuiDataGrid-cell--withRenderer MuiDataGrid-cell MuiDataGrid-cell--textLeft MuiDataGrid-withBorderColor" role="cell" data-field="reason" data-colindex="7" aria-colindex="8" aria-colspan="1" tabindex="-1" style="min-width: 120px; max-width: 120px; min-height: auto; max-height: none;">
-      <div class="jsx-1989257464 TableCell_TableCell__ofBYs " style="margin: inherit;">
-        <div class="jsx-1989257464 TableCell_container__RJgLi false ">
-          <div class="jsx-1989257464 " style="cursor: initial;">
-            <p class="jsx-1989257464">${reason}</p>
+      <div class=" TableCell_TableCell__ofBYs " style="margin: inherit;">
+        <div class=" TableCell_container__RJgLi false ">
+          <div class=" " style="cursor: initial;">
+            <p class="">${reason}</p>
           </div>
         </div>
       </div>
--- src/components/mockSymbol.ts
+++ src/components/mockSymbol.ts
@@ -1,12 +1,12 @@
 type Props = {
-    symbol: string
+  symbol: string;
 };
 
 const mockSymbol = (props: Props) => {
   return `
     <div class="MuiDataGrid-cell MuiDataGrid-cell--textLeft" role="cell" data-field="symbol" data-colindex="0">
-      <div class="jsx-1989257464 TableCell_TableCell__ofBYs">
-        <div class="jsx-1989257464 TableCell_container__RJgLi">
+      <div class=" TableCell_TableCell__ofBYs">
+        <div class=" TableCell_container__RJgLi">
           <p style="color: rgb(128, 128, 128);">${props.symbol}</p>
         </div>
       </div>
